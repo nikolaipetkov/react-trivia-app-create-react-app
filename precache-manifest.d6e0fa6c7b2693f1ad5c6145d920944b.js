d8c27ad697c6ff7add7338ed0d2ea8db4700bee0
Nikolay Petkov <nikolay.petkov@sirma.bg>
2019-04-12 00:10:37 +0300

    Updates

---
+++ precache-manifest.d6e0fa6c7b2693f1ad5c6145d920944b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-trivia-app-create-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b1eebd39d00615bf1d49",
+    "url": "/react-trivia-app-create-react-app/static/js/runtime~main.3b9f75c9.js"
+  },
+  {
+    "revision": "b8d260a9fc11b9d30c34",
+    "url": "/react-trivia-app-create-react-app/static/js/main.f375d913.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-trivia-app-create-react-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b8d260a9fc11b9d30c34",
+    "url": "/react-trivia-app-create-react-app/static/css/main.b9575f3e.chunk.css"
+  },
+  {
+    "revision": "a7161bb723e6056f6e1967f7c88a09e7",
+    "url": "/react-trivia-app-create-react-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-trivia-app-create-react-app/precache-manifest.d6e0fa6c7b2693f1ad5c6145d920944b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-trivia-app-create-react-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
